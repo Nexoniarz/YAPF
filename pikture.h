79a46b3ddf0a50597cdc028595c16ed82936551d
Nexoniarz <nekusoniaru@proton.me>
2026-05-13 20:17:24 +0000

    Update pikture.h

--- pikture.h
+++ pikture.h
@@ -1,7 +1,7 @@
 /*
 pikture.h
 Native PIKTURE format encoder and decoder library.
-Version: 100
+Version: 101
 License: MIT
 Made by: Nexoniarz
 */
@@ -12,34 +12,26 @@ Made by: Nexoniarz
 #include <stdint.h>
 #include <stddef.h>
 
-/* Image data structure */
 typedef struct {
     uint16_t width;
     uint16_t height;
-    uint8_t channels; /* Always 4 (RGBA) */
-    uint8_t* pixels;  /* Raw RGBA pixel array */
+    uint8_t channels;
+    uint8_t endianness;
+    uint8_t* pixels;
 } pikture_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* * Loads a PIKTURE image from the specified file path.
- * Returns a pointer to a pikture_t struct, or NULL on failure.
- */
 pikture_t* pikture_load(const char* filename);
 
-/* * Saves a PIKTURE image to the specified file path.
- * Returns 1 on success, 0 on failure.
- */
 int pikture_save(const char* filename, const pikture_t* img);
 
-/* * Frees the memory allocated for the PIKTURE image.
- */
 void pikture_free(pikture_t* img);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* PIKTURE_H */
+#endif
